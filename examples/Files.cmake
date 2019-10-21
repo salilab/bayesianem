d88592807dfbeb719cb65e1f4e796dda60d9befa
Ben Webb <ben@salilab.org>
2019-10-21 09:04:58 -0700

    Remove auto-generated file from source control

--- examples/Files.cmake
+++
@@ -1,3 +0,0 @@
-set(pyfiles "3D-example/input/session.py;3D-example/test_bayesian_gaussian_em_restraint.py;3D-example_2/input/bayesian_em_restraint/analysis/clustering.py;3D-example_2/input/session.py;3D-example_2/test_bayesian_gaussian_em_restraint.py;modeling.py")
-set(cppfiles "")
-set(cudafiles "")
